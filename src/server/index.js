5225cb7ef66467be6631cd4baf4a141884e29e7a
Dione Batista <dione1993@hotmail.com>
2024-04-24 17:28:16 -0300

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ let Self = {};
 VrpTunnel.bindInterface("vrp-js-boilerplate", Self);
 const ClientSide = VrpTunnel.getInterface("vrp-js-boilerplate");
 
-Self.AddMoney = (money) => {
+Self.SendValue = (money) => {
   let src = source;
   let user_id = vRP.getUserId(src);
   vRP.giveBankMoney(user_id, money);
